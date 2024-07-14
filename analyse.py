c0d73dbb477c073c90f34bfffa2b3568cc5169c5
trampoline <blchenx@163.com>
2024-07-14 18:51:40 +0800

    添加游戏文件和readme说明

---
+++ analyse.py
@@ -0,0 +1,130 @@
+import numpy as np
+import math as mt
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 防止画图中文报错
+import matplotlib
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+
+
+# 排列组合可以直接从math库中引用
+# 排列即A(k,n)，函数为 mt.perm(n, k=None)，n中选k
+# 组合即C(k,n)，函数为 mt.comb(n, k)，n中选k
+
+# 计算 组合-情况书-概率-奖金-各组别期望-总期望
+def st1():
+    # 总的可能数量
+    total = mt.comb(24, 12)  # 2704156
+    # print(total)
+
+    df0 = pd.DataFrame({'组合': ['总数'],
+                        '情况数': [mt.comb(24, 12)]})
+    # print(df0)
+    # 每种情况
+    # ① 三个数不同，乘数6
+    df1 = pd.DataFrame({'组合': ['840', '831', '750', '741', '732', '651', '642', '543'],
+                        '情况数': [6 * mt.comb(8, 8) * mt.comb(8, 4) * mt.comb(8, 0),
+                                   6 * mt.comb(8, 8) * mt.comb(8, 3) * mt.comb(8, 1),
+                                   6 * mt.comb(8, 7) * mt.comb(8, 5) * mt.comb(8, 0),
+                                   6 * mt.comb(8, 7) * mt.comb(8, 4) * mt.comb(8, 1),
+                                   6 * mt.comb(8, 7) * mt.comb(8, 3) * mt.comb(8, 2),
+                                   6 * mt.comb(8, 6) * mt.comb(8, 5) * mt.comb(8, 1),
+                                   6 * mt.comb(8, 6) * mt.comb(8, 4) * mt.comb(8, 2),
+                                   6 * mt.comb(8, 5) * mt.comb(8, 4) * mt.comb(8, 3)]})
+
+    # ② 两个数相同，乘数3
+    df2 = pd.DataFrame({'组合': ['822', '660', '633', '552'],
+                        '情况数': [3 * mt.comb(8, 8) * mt.comb(8, 2) * mt.comb(8, 2),
+                                   3 * mt.comb(8, 6) * mt.comb(8, 6) * mt.comb(8, 0),
+                                   3 * mt.comb(8, 6) * mt.comb(8, 3) * mt.comb(8, 3),
+                                   3 * mt.comb(8, 5) * mt.comb(8, 5) * mt.comb(8, 2)]})
+
+    # ③ 三个数相同，乘数1
+    df3 = pd.DataFrame({'组合': ['444'],
+                        '情况数': [1 * mt.comb(8, 4) * mt.comb(8, 4) * mt.comb(8, 4)]})
+
+    # 汇总并计算概率
+    df = pd.concat((df0, df1, df2, df3), ignore_index=True)
+    df['概率P'] = df['情况数'] / df['情况数'].iloc[0]
+    # df.reset_index(drop=True)
+    print(df)
+
+    # 添加中奖金额
+    money = pd.DataFrame({'组合': ['840', '831', '750', '741', '732', '651', '642',
+                                   '822', '660', '633', '552', '444', '543'],
+                          '奖金': [50, 10, 20, 2, 2, 1, 1, 10, 20, 1, 1, 1, -5]})
+    # 合并两表
+    df = pd.merge(df, money, how='left', on='组合')
+
+    # 计算期望
+    df['各组合期望'] = df['概率P'] * df['奖金']
+    df['总期望E'] = df['各组合期望'].sum()
+    # 按照情况数降序排列
+    df = df.sort_values(by=['情况数'], ascending=False)
+    # 重置索引index
+    df = df.reset_index(drop=True)
+    print(df)
+    return df
+
+
+# 把以上步骤封装为函数
+def moball_game(i):
+    # 总的可能数量
+    df0 = pd.DataFrame({'组合': ['总数'],
+                        '情况数': [mt.comb(24, 12)]})
+
+    # 每种情况
+    # ① 三个数不同，乘数6
+    df1 = pd.DataFrame({'组合': ['_840', '_831', '_750', '_741', '_732', '_651', '_642', '_543'],
+                        '情况数': [6 * mt.comb(8, 8) * mt.comb(8, 4) * mt.comb(8, 0),
+                                   6 * mt.comb(8, 8) * mt.comb(8, 3) * mt.comb(8, 1),
+                                   6 * mt.comb(8, 7) * mt.comb(8, 5) * mt.comb(8, 0),
+                                   6 * mt.comb(8, 7) * mt.comb(8, 4) * mt.comb(8, 1),
+                                   6 * mt.comb(8, 7) * mt.comb(8, 3) * mt.comb(8, 2),
+                                   6 * mt.comb(8, 6) * mt.comb(8, 5) * mt.comb(8, 1),
+                                   6 * mt.comb(8, 6) * mt.comb(8, 4) * mt.comb(8, 2),
+                                   6 * mt.comb(8, 5) * mt.comb(8, 4) * mt.comb(8, 3)]})
+
+    # ② 两个数相同，乘数3
+    df2 = pd.DataFrame({'组合': ['_822', '_660', '_633', '_552'],
+                        '情况数': [3 * mt.comb(8, 8) * mt.comb(8, 2) * mt.comb(8, 2),
+                                   3 * mt.comb(8, 6) * mt.comb(8, 6) * mt.comb(8, 0),
+                                   3 * mt.comb(8, 6) * mt.comb(8, 3) * mt.comb(8, 3),
+                                   3 * mt.comb(8, 5) * mt.comb(8, 5) * mt.comb(8, 2)]})
+
+    # ③ 三个数相同，乘数1
+    df3 = pd.DataFrame({'组合': ['_444'],
+                        '情况数': [1 * mt.comb(8, 4) * mt.comb(8, 4) * mt.comb(8, 4)]})
+
+    # 汇总并计算概率
+    df = pd.concat((df0, df1, df2, df3))
+    df['概率P'] = df['情况数'] / df['情况数'].iloc[0]
+
+    # 添加中奖金额
+    money = pd.DataFrame({'组合': ['_840', '_831', '_750', '_741', '_732', '_651', '_642', '_543',
+                                   '_822', '_660', '_633', '_552', '_444'],
+                          '奖金': [50, 10, 20, 2, 2, 1, 1, i, 10, 20, 1, 1, 1]})
+    # 合并两表
+    df = pd.merge(df, money, how='left', on='组合')
+
+    # 计算期望
+    df['各组合期望'] = df['概率P'] * df['奖金']
+    df['总期望E'] = df['各组合期望'].sum()
+    return df['总期望E'].iloc[0]
+
+
+if __name__ == '__main__':
+    st1()
+    # print(f'摸球游戏的平均期望：{moball_game(-5)}')
+    lis = np.linspace(-10, 0, 101)
+    x = [i for i in lis]
+    y = [moball_game(j) for j in lis]
+    x_y = [(round(k, 3), round(moball_game(k), 3)) for k in lis]
+    # print(x_y, sep='\n')
+
+    # 画图
+    plt.figure('x轴：组合_543 和 y轴：总期望 的关系')
+    plt.plot(x, y, color='olive')
+    # plt.show()
---
+++ main.py
@@ -0,0 +1,23 @@
+import moball_class as p
+
+
+if __name__ == '__main__':
+    # 玩家姓名
+    player_name = 'tram'
+    # 游戏模式，individual 单次式/epochs 批量式，输入错误时兼容为individual 单次式
+    play_model = 'epochs'
+    # 初始金额
+    init_amount = 100
+    # 每批次数量
+    batches = 6
+    # 将以上数据输入给程序，开始游玩~
+    player = p.MoBallGame(player_name, play_model,
+                          batches=batches, init_amount=init_amount,
+                          moball_model=5, tempt=True)
+    player.play_()
+    # 游戏结束后的画图分析
+    player.draw_analyse()
+    # 游戏结束后的数据汇总，输出为 csv 格式
+    player.info_print()
+    print('*' * 15, 'Done!', '*' * 15)
+
